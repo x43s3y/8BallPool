a3df3d7b19ce3244eba20859e2d07b36bf2c58ef
x43s3y <stefankosstov@gmail.com>
2023-11-22 07:02:17 +0200

    .

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import time
 import pygame
 from board import Board
 from player import Player
@@ -15,6 +16,7 @@ pygame.init()
 DISPLAY = pygame.display.set_mode((WIDTH, HEIGHT))
 clock = pygame.time.Clock()
 running = True
+aiming = False
 
 board = Board(BOARD_COLOR, BOARD_WIDTH, BOARD_HEIGHT, WIDTH, HEIGHT, DISPLAY)
 player = Player(DISPLAY, player_color, player_x, player_y)
@@ -29,8 +31,15 @@ while running:
         if event.type == pygame.QUIT:
             running = False
         elif event.type == pygame.MOUSEBUTTONDOWN:
-            player.check_if_clicked()
-            print(pygame.mouse.get_pos())
+            if DISPLAY.get_at(pygame.mouse.get_pos()) == player.color:
+                aiming = True
+
+        if aiming:
+            print(event.type)
+            if event.type == pygame.K_SPACE:
+                aiming = False
+            pygame.draw.line(DISPLAY, "black", (player.x, player.y), (pygame.mouse.get_pos()))
+            # time.sleep(0.01)
 
     pygame.display.update()
     clock.tick(FPS)
